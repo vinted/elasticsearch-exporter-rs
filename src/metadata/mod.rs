9e995f8a9eab0769030d8d07b5821761b749b7f0
Ernestas Poskus <ernestas.poskus@gmail.com>
2020-11-19 10:32:06 +0200

    metrics: add cluster version inject to NodeData
    Change-Id: I25189b50cddc5c72f7a4208bd6423a7be9906243

--- src/metadata/mod.rs
+++ src/metadata/mod.rs
@@ -3,7 +3,7 @@ use elasticsearch::{Elasticsearch, Error};
 use serde_json::Value;
 
 pub(crate) mod node_data;
-pub(crate) use node_data::{build, poll, IdToMetadata};
+pub(crate) use node_data::{build, poll, IdToMetadata, NodeData};
 
 pub(crate) async fn cluster_name(client: &Elasticsearch) -> Result<String, Error> {
     info!("Elasticsearch: fetching cluster_name");
--- src/metrics/_nodes/responses/mod.rs
+++ src/metrics/_nodes/responses/mod.rs
@@ -1,7 +1,7 @@
 use serde_json::Value;
 use std::collections::HashMap;
 
-use crate::metadata::IdToMetadata;
+use crate::metadata::{IdToMetadata, NodeData};
 
 /// Nodes response
 #[derive(Debug, Serialize, Deserialize)]
@@ -23,7 +23,7 @@ impl NodesResponse {
         // Inject node label
         for (node_id, mut data) in self.nodes.drain() {
             if let Some(node_metadata) = metadata_read.get(&node_id) {
-                inject_label(&mut data, &node_metadata.name, keys_to_remove);
+                inject_label(&mut data, &node_metadata, keys_to_remove);
 
                 values.push(data);
             }
@@ -33,9 +33,13 @@ impl NodesResponse {
     }
 }
 
-fn inject_label(value: &mut Value, name: &str, keys_to_remove: &[&'static str]) {
+fn inject_label(value: &mut Value, node_data: &NodeData, keys_to_remove: &[&'static str]) {
     if let Some(map) = value.as_object_mut() {
-        let _ = map.insert("name".into(), Value::String(name.to_string()));
+        let _ = map.insert("name".into(), Value::String(node_data.name.to_string()));
+        let _ = map.insert(
+            "cluster_version".into(),
+            Value::String(node_data.version.to_string()),
+        );
 
         // Doing inverse removal because serde_json::Map does not have .retain
         for to_remove in keys_to_remove {
@@ -43,12 +47,12 @@ fn inject_label(value: &mut Value, name: &str, keys_to_remove: &[&'static str]) 
         }
 
         for (_, object_value) in map {
-            inject_label(object_value, name, keys_to_remove);
+            inject_label(object_value, node_data, keys_to_remove);
         }
     }
     if let Some(array) = value.as_array_mut() {
         for object_array in array {
-            inject_label(object_array, name, keys_to_remove);
+            inject_label(object_array, node_data, keys_to_remove);
         }
     }
 }
