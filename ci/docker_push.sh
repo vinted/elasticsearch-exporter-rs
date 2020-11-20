8e83bc3e6f07ba4e6f876f58a26a19eebbe190bc
Ernestas Poskus <ernestas.poskus@gmail.com>
2020-11-20 23:17:23 +0200

    ci: add docker_push docker tag to image
    Change-Id: Ib33c5a82312eb69a67eb2617ac668bb0f30a83b6

--- ci/docker_push.sh
+++ ci/docker_push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-TAG=ernestasvinted/elasticsearch_exporter
+GIT_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+DOCKER_TAG=${TRAVIS_TAG:-$GIT_TAG}
+
+TAG="ernestasvinted/elasticsearch_exporter:$DOCKER_TAG"
 
 docker build -t $TAG .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
