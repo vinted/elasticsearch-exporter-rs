f6eb1c57166dca659f35507f1fc22d350d7d8775
Ernestas Poskus <ernestas.poskus@gmail.com>
2020-12-10 13:39:09 +0200

    metrics: add info metric to /_cat/aliases
    Change-Id: If5278190aabfa43b6411a40bb44615bf320b1253

--- src/metrics/_cat/aliases.rs
+++ src/metrics/_cat/aliases.rs
@@ -1,4 +1,7 @@
 use elasticsearch::cat::CatAliasesParts;
+use serde_json::Map as SerdeMap;
+
+use super::responses::CatResponse;
 
 pub(crate) const SUBSYSTEM: &'static str = "cat_aliases";
 
@@ -15,7 +18,37 @@ async fn metrics(exporter: &Exporter) -> Result<Vec<Metrics>, elasticsearch::Err
         .send()
         .await?;
 
-    Ok(metric::from_values(response.json::<Vec<Value>>().await?))
+    let values = response
+        .json::<CatResponse>()
+        .await?
+        .into_values(inject_cat_aliases_info);
+
+    Ok(metric::from_values(values))
+}
+
+fn inject_cat_aliases_info(map: &mut SerdeMap<String, Value>) {
+    let is_system_index: bool = map
+        .get("index")
+        .map(|index| index.as_str().map(|s| s.starts_with(".")).unwrap_or(false))
+        .unwrap_or(false);
+
+    if is_system_index {
+        map.clear();
+    } else {
+        let _ = map.insert("info".into(), Value::from(1));
+    }
 }
 
 crate::poll_metrics!();
+
+#[tokio::test]
+async fn test_cat_aliases() {
+    let cat: CatResponse = serde_json::from_str(include_str!("../../tests/files/cat_aliases.json"))
+        .expect("valid json");
+
+    let got = cat.into_values(inject_cat_aliases_info);
+
+    assert_eq!(got.len(), 52);
+
+    assert_eq!(got[0]["info"], 1);
+}
